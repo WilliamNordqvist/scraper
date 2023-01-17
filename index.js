4bd1acfa88b5c3f94aac2d7d6a2d6e6613bfeab7
William Nordqvist <williamnordqvist@MacBook-Pro-som-tillhor-William.local>
2023-01-17 11:22:17 +0100

    install express

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const cron = require("node-cron");
 const axios = require("axios");
+const app = require("express")()
 
-cron.schedule("*/1 * * * * *", () => {
-  axios.post(
-    "https://script.google.com/macros/s/AKfycbxpTRsgmIxbR_MMcjUCzans00TGaHLOmIADVl1rZ9RBQbOnIFLbg6RR7O7cwr-wrhI6NA/exec",
-    ["from code"]
-  );
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || 3000
 
-  console.log("running a task every 10 second");
-});
+
+
+
+app.listen(port, () => {
+
+  cron.schedule("*/1 * * * * *", () => {
+    axios.post(
+      "https://script.google.com/macros/s/AKfycbxpTRsgmIxbR_MMcjUCzans00TGaHLOmIADVl1rZ9RBQbOnIFLbg6RR7O7cwr-wrhI6NA/exec",
+      ["from code"]
+    );
+  
+    console.log("running a task every 10 second");
+  });
+
+})
