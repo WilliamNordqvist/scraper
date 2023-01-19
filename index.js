d5142d3c5856f1208c3826cc279340775959a82f
William Nordqvist <williamnordqvist@MacBook-Pro-som-tillhor-William.local>
2023-01-19 14:20:44 +0100

    troubleshooting capacity

--- index.js
+++ index.js
@@ -4,41 +4,36 @@ const axios = require("axios");
 const app = require("express")();
 const cheerio = require("cheerio");
 const { format } = require("date-fns");
-
-require("dotenv").config();
-
 const port = process.env.PORT || 3000;
-
-const scraper = async () => {
-  const response = await axios.get("https://www.klart.se/se/");
-
-  const html = response.data;
-  const $ = cheerio.load(html);
-  const allCities = $(".place-item div");
-
-  const mappedCities = allCities.map((i, city) => {
-    const cityName = $(city).find("h4").text();
-    const temp = $(city).find(".temp").text().replace(/°/g, "");
-
-    return {
-      name: cityName,
-      temp,
-    };
-  });
-
-  return {
-    data: mappedCities.toArray(),
-    date: format(new Date(), "yyyy/MM/dd"),
-  };
-};
 
 app.listen(port, () => {
   console.log("SERVER STARTED");
 
-  cron.schedule( "00 00 12 * * *", async () => {
+  cron.schedule(
+    "00 00 12 * * *",
+    async () => {
+      require("dotenv").config();
       console.log("Fetching data");
-      const res = await scraper(); 
-      axios.post(process.env.URL, res);
+      const response = await axios.get("https://www.klart.se/se/");
+
+      const html = response.data;
+      const $ = cheerio.load(html);
+      const allCities = $(".place-item div");
+
+      const mappedCities = allCities.map((i, city) => {
+        const cityName = $(city).find("h4").text();
+        const temp = $(city).find(".temp").text().replace(/°/g, "");
+
+        return {
+          name: cityName,
+          temp,
+        };
+      });
+
+      axios.post(process.env.URL, {
+        data: mappedCities.toArray(),
+        date: format(new Date(), "yyyy/MM/dd"),
+      });
     },
     {
       scheduled: true,
